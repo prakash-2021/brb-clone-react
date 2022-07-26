23ce6756dc16a66a55b1b047bbb628599711fd1e
prakash-2021 <prakashrestha2075@gmail.com>
2022-07-26 16:36:11 +0545

    fixed #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Hero(props) {
+export default function Hero({ title }) {
   return (
     <section className="hero">
       <img src="/images/photo-grid.png" className="hero--img" />
-      <h1 className="hero--header">{props.title}</h1>
+      <h1 className="hero--header">{title}</h1>
       <p className="hero--text">
         Join unique iteractive activities led by one-of-a-kind hosts without
         leaving home.
