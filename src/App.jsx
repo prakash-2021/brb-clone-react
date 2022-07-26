e952556d1f025bc38efa36cfca26a78d3bb96bc3
prakash-2021 <prakashrestha2075@gmail.com>
2022-07-26 15:50:27 +0545

    added

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
+
+export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        {...item}
+      />
+    );
+  });
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <section className="cards-lists">{cards}</section>
+    </>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Card(props) {
+    console.log(props)
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  console.log()
+  return (
+    <div className="card">
+      {badgeText && <div className="card__badge">{badgeText}</div>}
+      <img src={`/images/${props.coverImg}`} className="card__img-main" />
+      <div className="card__flex">
+        <img src="/images/star.png" className="card__star" />
+        <span className="card__rate-number">{props.stats.rating}</span>
+        <span className="card__text-light">({props.stats.reviewCount})</span>
+        <span className="card__text-light">.</span>
+        <span className="card__text-light">{props.location}</span>
+      </div>
+      <p className="card__description">{props.title}</p>
+      <strong className="card__price-tag">
+        From ${props.price} <span className="card__text-light2">/ person</span>
+      </strong>
+    </div>
+  );
+}
